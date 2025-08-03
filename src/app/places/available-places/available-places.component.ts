9cd9281f94a062d2cbfdbfb16d0d4ce77eca5f2d
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-03 20:45:55 +0530

    we can catch an error either in pipe, or write the error-handling code in error() in subsrcibe, ideally we should aim to keep the code for susbcribe() methods lean, catchError() from rxjs can be useful in such cases

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -17,13 +17,25 @@ export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   private httpClient = inject(HttpClient);
   isFetching = signal(false);
+  error = signal('');
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
     this.isFetching.set(true);
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
-      .pipe(map((resData) => resData.places))
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching the available places. Please try again later.'
+              )
+          );
+        })
+      )
       .subscribe({
         next: (places) => {
           console.log(places);
@@ -32,7 +44,13 @@ export class AvailablePlacesComponent implements OnInit {
         complete: () => {
           this.isFetching.set(false);
         },
-        error: () => {},
+        error: (error: Error) => {
+          this.error.set(error.message);
+          // console.log(error);
+          // this.error.set(
+          //   'Something went wrong fetching the available places. Please try again later.'
+          // );
+        },
       });
 
     // const subscription = this.httpClient
