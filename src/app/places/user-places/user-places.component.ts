26131d1122af714843edb82ad182b25bcfd1a677
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-04 12:51:36 +0530

    a lot of code duplication from available-places component into user-places, the logic can be instead put into service and then service used

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { catchError, map, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +13,73 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+  isFetching = signal(false);
+  userPlaces = signal<Place[] | undefined>(undefined);
+  error = signal('');
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.isFetching.set(true);
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/user-places')
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching your favourite places. Please try again later.'
+              )
+          );
+        })
+      )
+      .subscribe({
+        next: (places) => {
+          console.log(places);
+          this.userPlaces.set(places);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+          // console.log(error);
+          // this.error.set(
+          //   'Something went wrong fetching the available places. Please try again later.'
+          // );
+        },
+      });
+
+    // const subscription = this.httpClient
+    //   .get<{ places: Place[] }>('http://localhost:3000/places', {
+    //     observe: 'response',
+    //   })
+    //   .subscribe({
+    //     next: (response) => {
+    //       console.log(response);
+    //       console.log(response.body?.places);
+    //     },
+    //     complete: () => {},
+    //     error: () => {},
+    //   });
+
+    // const subscription = this.httpClient
+    //   .get<{ places: Place[] }>('http://localhost:3000/places', {
+    //     observe: 'events',
+    //   })
+    //   .subscribe({
+    //     next: (event) => {
+    //       console.log(event);
+    //     },
+    //     complete: () => {},
+    //     error: () => {},
+    //   });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
