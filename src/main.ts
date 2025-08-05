4d16160add31af66c48d5938077994ac379029b0
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-05 11:38:24 +0530

    added interceptor that works on incoming response using pipe + tap operator, don't use subscribe, use tap

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import {
+  HttpEventType,
   HttpHandlerFn,
   HttpRequest,
   provideHttpClient,
   withInterceptors,
 } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(
   request: HttpRequest<unknown>,
@@ -18,7 +20,18 @@ function loggingInterceptor(
   console.log('[Outgoing Request]');
   console.log(request);
   // console.log(req);
-  return next(request);
+  return next(request).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('[Incoming Response]');
+          console.log(event);
+          console.log(event.status);
+          console.log(event.body);
+        }
+      },
+    })
+  );
 }
 bootstrapApplication(AppComponent, {
   providers: [provideHttpClient(withInterceptors([loggingInterceptor]))],
